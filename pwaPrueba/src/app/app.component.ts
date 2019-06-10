72dbb69ed0e32c230929de6a8b7a1350cef332e4
Mateo Ortiz Cano - Ceiba <mateo.ortiz@ceiba.com.co>
2019-06-10 15:47:09 -0500

    Se realiza método que ayuda a que se actualice la versión del service worker de manera automática cuando se realicen actualizaciones.

--- pwaPrueba/src/app/app.component.ts
+++ pwaPrueba/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
-  title = 'pwaPrueba';
+export class AppComponent implements OnInit {
+  title = 'Prueba PWA con Angular';
+
+  constructor(private swUpdate: SwUpdate) {
+
+  }
+
+  ngOnInit(): void {
+    // Este método ayuda a que se actualice la versión del service worker de manera automática cuando se realicen actualizaciones
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        window.location.reload();
+      });
+    }
+  }
 }
